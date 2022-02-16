89261ed8e8f5b539045f8fcc6579d90a68647aa4
harshcrop <harshcrop@gmail.com>
2022-02-16 13:39:35 +0530

    layput file created

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+
+class layout extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="">
+        <Header />
+        <div className="pb-12">{this.props.children}</div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default layout;
